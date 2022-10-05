30cd56de1989ed096455561a5f53571f3253af01
thijmen <PUBGtjes@gmail.com>
2022-10-05 11:42:58 +0200

    Initial commit nr 2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class main
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("You need to add folders/files like:  Freddy folder1 folder2 file1.txt file2.txt");
+            return;
+        }
 
-Console.WriteLine("Hello, World!");
+        foreach (string s in args)
+        {
+            if (s.Contains('.'))
+            {
+                file(Directory.GetCurrentDirectory(), s);
+            }
+            else
+            {
+                folder(s);
+            }
+        }
+    }
+
+    private static void folder(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+
+    private static void file(string path, string name)
+    {
+        string PathString = path + "/" + name;
+        if (!File.Exists(PathString))
+        {
+            File.Create(PathString, 0);
+        }
+    }
+}
